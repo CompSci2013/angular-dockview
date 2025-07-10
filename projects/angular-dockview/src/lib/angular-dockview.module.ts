c42d27e9d868bc3f5808da44da3b9d81b00186e7
dev1 <dev1@homelab.com>
2025-07-09 20:43:24 -0400

    first commit

---
+++ projects/angular-dockview/src/lib/angular-dockview.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AngularDockviewComponent } from './angular-dockview.component';
+
+
+
+@NgModule({
+  declarations: [
+    AngularDockviewComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AngularDockviewComponent
+  ]
+})
+export class AngularDockviewModule { }
